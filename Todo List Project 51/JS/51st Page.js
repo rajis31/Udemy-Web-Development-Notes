1528d369055e2328f5eb1eba1ebea9310c066d8c
unknown <raj350@mail.fresnostate.edu>
2018-08-08 23:26:47 -0700

    Batch 8-8-2018

--- Todo List Project 51/JS/51st Page.js
+++ Todo List Project 51/JS/51st Page.js
@@ -19,6 +19,31 @@
 // }
 // });
 
-$("li").on("click",function(){
+$("ul").on("click","li",function(){ //What is happen here is that when any li is clicked inside the UL, then it will trigger the function for the entire ul.
     $(this).toggleClass("completed");
+});
+
+//Click on X to delete todo
+//later changed from click to on to allow for any new elements added to ul to be deleted
+$("ul").on("click","span",function(event){ //Since span is inside of UL which is inslde of Div , if ul and div have event listeners for click lets say, when the span is clicked on, the ul and div events will trrigger as well which is call event bubbling
+ $(this).parent().fadeOut(500, function(){ //this refers to the li and not not the span
+   $(this).remove();
+ }); //removes the li
+  event.stopPropagation(); //This whill stop event bubbling
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which===13){
+    //grabbiung todo text from input
+    var todoText=$(this).val();
+    //clears the input box
+    $(this).val("");
+    //Create new LI and add to UL
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span ></span> "+todoText +"</li>");
+
+  }
+});
+
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
 });
